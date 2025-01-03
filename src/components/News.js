c73932e6389861e1ac3e5496933ef90d9e8ccb4d
Pranay-Rokade <pranayrokade86@gmail.com>
2025-01-03 21:42:33 +0530

    Added author name, date and source in the News card

--- src/components/News.js
+++ src/components/News.js
@@ -68,7 +68,7 @@ export class News extends Component {
         <div className="row my-2">
         {!this.state.loading && this.state.articles.map((element) => {
           return <div className="col-md-4" key={element.url}>
-          <NewsItem title={element.title?element.title:""} description={element.description?element.description.slice(0,88):""} imageUrl={element.urlToImage} newsUrl={element.url}/>
+          <NewsItem title={element.title?element.title:""} description={element.description?element.description.slice(0,88):""} imageUrl={element.urlToImage} newsUrl={element.url} author={element.author} date={element.publishedAt} source={element.source.name}/>
         </div>
         })}
         </div>
